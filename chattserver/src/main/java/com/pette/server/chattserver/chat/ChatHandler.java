86475553c5eec23271ec83beb7551d65b5685f2c
Pette <petrit_gerdovci@hotmail.com>
2016-12-10 20:10:09 +0100

    Minor hacks to fix chatting with client app

--- chattserver/src/main/java/com/pette/server/chattserver/chat/ChatHandler.java
+++ chattserver/src/main/java/com/pette/server/chattserver/chat/ChatHandler.java
@@ -1,61 +1,68 @@
 package com.pette.server.chattserver.chat;
 
-import com.pette.server.common.UpdateRequest;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.LinkedHashMap;
+
+import com.pette.server.common.UpdateRequest;
 
 public class ChatHandler {
-    private ConcurrentHashMap<String, ChatRoom> chatRooms;
+    private LinkedHashMap<String, ChatRoom> chatRooms;
     private static ChatHandler instance = null;
 
     private ChatHandler() {
-        chatRooms = new ConcurrentHashMap<>();
-        ChatRoom chatRoom1 = new ChatRoom(new ArrayList(Arrays.asList("sebbe", "pette")));
-        chatRooms.put("0", chatRoom1);
+	chatRooms = new LinkedHashMap<>();
+	ChatRoom chatRoom1 = new ChatRoom(new ArrayList(Arrays.asList("sebbe",
+		"pette")));
+	chatRooms.put("0", chatRoom1);
     }
 
     public static ChatHandler getInstance() {
-        if (instance == null) {
-            instance = new ChatHandler();
-        }
-        return instance;
+	if (instance == null) {
+	    instance = new ChatHandler();
+	}
+	return instance;
     }
 
     public void handleNewMessage(ChatMessage message, String chatRoomId) {
-        ChatRoom room = chatRooms.get(chatRoomId);
-        if (room == null) {
-            return;
-        }
-        room.addMessage(message);
+	ChatRoom room = chatRooms.get(chatRoomId);
+	if (room == null) {
+	    return;
+	}
+	room.addMessage(message);
     }
 
-    private ArrayList<ChatMessage> getSlimUpdate(String chatRoomId, String userName) {
-        if (chatRooms.get(chatRoomId) != null) {
-            return chatRooms.get(chatRoomId).getMessagesForUserAndUpdateIndex(userName);
-        }
-        return new ArrayList<>();
+    private ArrayList<ChatMessage> getSlimUpdate(String chatRoomId,
+	    String userName) {
+	if (chatRooms.get(chatRoomId) != null) {
+	    return chatRooms.get(chatRoomId).getMessagesForUserAndUpdateIndex(
+		    userName);
+	}
+	return new ArrayList<>();
     }
 
     public ArrayList<ChatMessage> getUpdate(UpdateRequest request) {
-        if (request.getPuzzle() != null) {
-            return getAfterTimeStampEntries(request.getChatRoomId(), request.getPuzzle());
-        } else if (request.getIndex() != null && request.getRange() != null) {
-            return getLastEntries(request.getChatRoomId(), request.getIndex(), request.getRange());
-        } else {
-            return getSlimUpdate(request.getChatRoomId(), request.getUsername());
-        }
+	if (request.getPuzzle() != null) {
+	    return getAfterTimeStampEntries(request.getChatRoomId(),
+		    request.getPuzzle());
+	} else if (request.getIndex() != null && request.getRange() != null) {
+	    return getLastEntries(request.getChatRoomId(), request.getIndex(),
+		    request.getRange());
+	} else {
+	    return getSlimUpdate(request.getChatRoomId(), request.getUsername());
+	}
     }
 
-    private ArrayList<ChatMessage> getLastEntries(String chatRoomId, int index, int range) {
-        ChatRoom room = chatRooms.get(chatRoomId);
-        return room.getLastMessages(index, range);
+    private ArrayList<ChatMessage> getLastEntries(String chatRoomId, int index,
+	    int range) {
+	ChatRoom room = chatRooms.get(chatRoomId);
+	return room.getLastMessages(index, range);
     }
 
-    private ArrayList<ChatMessage> getAfterTimeStampEntries(String chatRoomId, Date date) {
-        ChatRoom room = chatRooms.get(chatRoomId);
-        return room.getAfterTimeStampMessages(date);
+    private ArrayList<ChatMessage> getAfterTimeStampEntries(String chatRoomId,
+	    Date date) {
+	ChatRoom room = chatRooms.get(chatRoomId);
+	return room.getAfterTimeStampMessages(date);
     }
 }
--- chattserver/src/main/java/com/pette/server/chattserver/chat/ChatRoom.java
+++ chattserver/src/main/java/com/pette/server/chattserver/chat/ChatRoom.java
@@ -1,10 +1,7 @@
 package com.pette.server.chattserver.chat;
 
-import com.pette.server.common.*;
-
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class ChatRoom {
@@ -12,73 +9,79 @@ public class ChatRoom {
     ArrayList<ChatMessage> messages;
     ConcurrentHashMap<String, Integer> indexes;
 
-
     public ChatRoom(ArrayList<String> users) {
-        this.users = users;
-        indexes = new ConcurrentHashMap<>();
-        for (String user : users) {
-            indexes.put(user, 0);
-        }
-        messages = new ArrayList<>();
+	this.users = users;
+	indexes = new ConcurrentHashMap<>();
+	for (String user : users) {
+	    indexes.put(user, 0);
+	}
+	messages = new ArrayList<>();
     }
 
     public ArrayList<ChatMessage> getMessagesForUserAndUpdateIndex(String user) {
-        Integer index = indexes.get(user);
-        if (index != null) {
-            ArrayList<ChatMessage> subList = new ArrayList<>(messages.subList(index, messages.size() - 1));
-            indexes.put(user, messages.size());
-            return subList;
-        }
-        return null;
+	Integer index = indexes.get(user);
+	if (index != null && messages.size() > 1) {
+	    ArrayList<ChatMessage> subList = new ArrayList<>(messages.subList(
+		    index, messages.size() - 1));
+	    // indexes.put(user, messages.size());
+	    return subList;
+	} else {
+	    return messages;
+	}
     }
 
     public ArrayList<ChatMessage> getAfterTimeStampMessages(Date date) {
-        ArrayList<ChatMessage> returnList = new ArrayList<>();
-        for (ChatMessage message : messages) {
-            if (message.getTimeStamp().after(date) || message.getTimeStamp().equals(date)) {
-                returnList.add(message);
-            }
-        }
-        return returnList;
+	ArrayList<ChatMessage> returnList = new ArrayList<>();
+	for (ChatMessage message : messages) {
+	    System.out.println("###" + message.getMessageBody() + " "
+		    + message.getTimeStamp());
+	    if (message.getTimeStamp().after(date)) {
+		System.out.println("##" + date);
+		System.out.println("###after " + message.getMessageBody() + " "
+			+ message.getTimeStamp());
+		returnList.add(message);
+	    }
+	}
+	return returnList;
     }
 
     public ArrayList<ChatMessage> getLastMessages(int index, int range) {
-        ArrayList<ChatMessage> returnMessages = new ArrayList<>();
-        if (index < 0) {
-            index = 0;
-        }
-        if (index > messages.size()) {
-            index = messages.size();
-        }
-        if (range > messages.size()) {
-            range = messages.size();
-        }
-        if (index + range >= messages.size()) {
-            range = messages.size();
-        }
-        for (int i = index + range; i > index; i--) {
-            returnMessages.add(messages.get(messages.size() - i));
-        }
-        return returnMessages;
+	ArrayList<ChatMessage> returnMessages = new ArrayList<>();
+	if (index < 0) {
+	    index = 0;
+	}
+	if (index > messages.size()) {
+	    index = messages.size();
+	}
+	if (range > messages.size()) {
+	    range = messages.size();
+	}
+	if (index + range >= messages.size()) {
+	    range = messages.size();
+	}
+	for (int i = index + range; i > index; i--) {
+	    returnMessages.add(messages.get(messages.size() - i));
+	}
+	return returnMessages;
     }
 
     public void addMessage(ChatMessage message) {
-        messages.add(message);
+	messages.add(message);
     }
 
     public ArrayList<String> getUsers() {
-        return users;
+	return users;
     }
 
     public void setUsers(ArrayList<String> users) {
-        this.users = users;
+	this.users = users;
     }
 
     public ArrayList<ChatMessage> getMessages() {
-        return messages;
+	return messages;
     }
 
     public void setMessages(ArrayList<ChatMessage> messages) {
-        this.messages = messages;
+	this.messages = messages;
     }
 }
